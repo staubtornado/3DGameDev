8632370ec10ad01780d7f1253898f71bfc518ef5
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-07-23 13:28:23 +0200

    Improved camera movement

--- src/main/java/org/game3d/dev/engine/scene/Camera.java
+++ src/main/java/org/game3d/dev/engine/scene/Camera.java
@@ -6,60 +6,74 @@ import org.joml.Vector3f;
 import org.joml.Vector2f;
 
 public class Camera {
-    private final Vector3f direction;
     @Getter private final Vector3f position;
     private final Vector2f rotation;
-    private final Vector3f up;
     private final Vector3f right;
     @Getter private final Matrix4f viewMatrix;
 
     public Camera() {
-        this.direction = new Vector3f();
         this.position = new Vector3f();
         this.rotation = new Vector2f();
-        this.up = new Vector3f();
         this.right = new Vector3f();
         this.viewMatrix = new Matrix4f();
+        this.recalculate();
     }
 
     public void addRotation(float x, float y) {
-        this.rotation.add(x, y);
-        this.recalculate();
-    }
+        if (this.rotation.x + x > Math.PI / 2) {  // Prevent camera from going upside down
+            this.rotation.x = (float) (Math.PI / 2);
+        } else if (this.rotation.x + x < -Math.PI / 2) {
+            this.rotation.x = (float) (-Math.PI / 2);
+        } else {
+            this.rotation.x += x;
+        }
+        this.rotation.y += y;
 
-    public void moveBackwards(float distance) {
-        this.viewMatrix.positiveZ(this.direction).negate().mul(distance);
-        this.position.sub(this.direction);
-        this.recalculate();
-    }
-
-    public void moveDown(float distance) {
-        this.viewMatrix.positiveY(this.up).mul(distance);
-        this.position.sub(this.up);
+        if (this.rotation.y > 2 * Math.PI) {  // Keep rotation within 0 to 2*PI
+            this.rotation.y = y;
+        } else if (this.rotation.y < 0) {
+            this.rotation.y = (float) (2 * Math.PI);
+        }
         this.recalculate();
     }
 
     public void moveForward(float distance) {
-        this.viewMatrix.positiveZ(this.direction).negate().mul(distance);
-        this.position.add(this.direction);
+        double x = Math.sin(this.rotation.y) * -1;
+        double z = Math.cos(this.rotation.y);
+
+        Vector3f direction = new Vector3f((float) x, 0, (float) z).mul(distance).negate();
+        this.position.add(direction);
+        this.recalculate();
+    }
+
+    public void moveBackwards(float distance) {
+        double x = Math.sin(this.rotation.y) * -1;
+        double z = Math.cos(this.rotation.y);
+
+        Vector3f direction = new Vector3f((float) x, 0, (float) z).mul(distance).negate();
+        this.position.sub(direction);
+        this.recalculate();
+    }
+
+    public void moveUp(float distance) {
+        this.position.y += distance;
+        this.recalculate();
+    }
+
+    public void moveDown(float distance) {
+        this.position.y -= distance;
         this.recalculate();
     }
 
     public void moveLeft(float distance) {
         this.viewMatrix.positiveX(this.right).mul(distance);
-        this.position.sub(this.right);
+        this.position.sub(this.right.x, 0, this.right.z);
         this.recalculate();
     }
 
     public void moveRight(float distance) {
         this.viewMatrix.positiveX(this.right).mul(distance);
-        this.position.add(this.right);
-        this.recalculate();
-    }
-
-    public void moveUp(float distance) {
-        this.viewMatrix.positiveY(this.up).mul(distance);
-        this.position.add(this.up);
+        this.position.add(this.right.x, 0, this.right.z);
         this.recalculate();
     }
 
